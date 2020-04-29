4bf6fa5392df264d17afd8b6a6ab49dddd8933a8
mayuriktr <c14180078@john.petra.ac.id>
2020-04-29 08:53:49 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import {OperatorComponent} from './operator/operator.component';
 export class AppComponent  {
   data : String;
   number = 0;
-  
+  angkainputan = "";
   name = 'Kevin Reynaldi';
 
  
@@ -22,8 +22,62 @@ export class AppComponent  {
 
   angka7(){
     this.number = 7;
-    
+    this.angkainputan += "7";
 
   }
 
+  angka8(){
+    this.number = 8;
+    this.angkainputan += "8";
+
+  }
+    angka9(){
+    this.number = 9;
+    this.angkainputan += "9";
+
+  }
+    angka4(){
+    this.number = 4;
+    this.angkainputan += 4;
+
+  }
+    angka5(){
+    this.number = 5;
+    this.angkainputan += "5";
+
+  }
+    angka6(){
+    this.number = 6;
+    this.angkainputan += "6";
+
+  }
+    angka1(){
+    this.number = 1;
+    this.angkainputan += "1";
+
+  }
+
+    angka2(){
+    this.number = 2;
+    this.angkainputan += "2";
+
+  }
+
+    angka3(){
+    this.number = 3;
+    this.angkainputan += "3";
+
+  }
+
+  minus(){
+    this.angkainputan += "-";
+  }
+  plus(){
+      this.angkainputan += "+";
+
+  }
+  
+  kali(){
+      this.angkainputan += "*";
+  }
 }
